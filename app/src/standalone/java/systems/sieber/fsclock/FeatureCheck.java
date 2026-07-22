43962fbd604815e7aa5812fc676ee734f97275bb
ghegem <gh.ghem@aesopsfables.net>
2026-07-22 22:16:12 +0800

    Update FeatureCheck.java

--- app/src/standalone/java/systems/sieber/fsclock/FeatureCheck.java
+++ app/src/standalone/java/systems/sieber/fsclock/FeatureCheck.java
@@ -14,6 +14,7 @@ public class FeatureCheck extends BaseFeatureCheck {
 
         // no additional purchase store checks
         isReady = true;
+        unlockedSettings = true;
         if(listener != null) {
             listener.featureCheckReady(true);
         }
